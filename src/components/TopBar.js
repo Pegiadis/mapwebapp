3185674e37f7c39ab56ecbdbca32057217bcbe3e
Pegiadis <ioannispegiadis97@gmail.com>
2024-01-31 01:12:45 +0200

    Add icons

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi'; // Import the burger menu icon
 
 const TopBar = ({ togglePanel }) => {
     return (
         <nav className="bg-gray-800 p-4 flex justify-between items-center">
             <button onClick={togglePanel} className="text-white">
-                {/* Here you would include your burger menu icon */}
-                <span>Menu</span>
+                <GiHamburgerMenu size={24} /> {/* Use the icon here */}
             </button>
             <span className="text-white text-lg font-semibold">Your Application</span>
             {/* Rest of your top bar */}
